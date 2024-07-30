d6ef30cc895b698a051db13d963866a3144e3cb5
szierke <sascha.zierke@outlook.com>
2024-07-30 13:21:09 +0200

    Fix favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,21 @@ export default function RootLayout({
       className="scroll-smooth overflow-x-hidden"
       content="width=device-width, initial-scale=1.0"
     >
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         {children}
