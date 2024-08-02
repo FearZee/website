9676e4b4dce6225e09ebc2981f56eb900bce25a1
szierke <sascha.zierke@outlook.com>
2024-08-02 14:06:54 +0200

    Update logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -15,6 +15,8 @@ import { Drawer, DrawerContent, DrawerTrigger } from "./Drawer";
 import { IconMenu } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import classNames from "classnames";
+import logo from "../../../public/logo.svg";
+import Image from "next/image";
 
 export const Header = () => {
   const isDesktop = useMediaQuery("(min-width: 1024px)");
@@ -52,7 +54,7 @@ export const Header = () => {
               href={"/"}
               onClick={() => setOpen(false)}
             >
-              N|A
+              <Image src={logo} alt="Logo" />
             </a>
             <a href="/" onClick={() => setOpen(false)}>
               Saas Platform
@@ -73,73 +75,79 @@ export const Header = () => {
   }
 
   return (
-    <header className={classNames("w-full bg-zinc-900/70 text-white fixed top-0 z-10 transition-all duration-200 backdrop-blur-sm", {'translate-y-[-2rem]': !isVisible})}>
-      {isVisible &&
-      <div className="flex justify-center items-center p-4">
-        <Link className="text-3xl  absolute left-4" href={"/"}>
-          N|A
-        </Link>
-        <NavigationMenu>
-          <NavigationMenuList className="">
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink className="no-underline font-medium">
-                    Saas Platform
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent className="list-none bg-zinc-800">
-                <NavigationMenuItem className="p-4 w-96">
-                  <Link href="#how-does-it-work" legacyBehavior passHref>
+    <header
+      className={classNames(
+        "w-full bg-zinc-900/70 text-white fixed top-0 z-10 transition-all duration-200 backdrop-blur-sm",
+        { "translate-y-[-2rem]": !isVisible }
+      )}
+    >
+      {isVisible && (
+        <div className="flex justify-center items-center p-4">
+          <Link className="text-3xl  absolute left-4" href={"/"}>
+            <Image src={logo} alt="Logo" className="w-14" />
+          </Link>
+          <NavigationMenu>
+            <NavigationMenuList className="">
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <Link href="/" legacyBehavior passHref>
                     <NavigationMenuLink className="no-underline font-medium">
-                      Wie funktioniert es?
-                      <p className="text-xs font-normal">
-                        Wie ist der Workflow von der Anfrage bis zum Ergebnis?
-                      </p>
+                      Saas Platform
                     </NavigationMenuLink>
                   </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem className="p-4 w-96 pt-0">
-                  <Link href="/#our-mission" legacyBehavior passHref>
-                    <NavigationMenuLink className="no-underline font-medium">
-                      Das macht uns besonderns
-                      <p className="text-xs font-normal">
-                        Wir wollen Unternehmen in allen Belangen eine Hilfe sein
-                        für Ihren Erfolg.
-                      </p>
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem className="p-4 w-96 pt-0">
-                  <Link href="#our-ai" legacyBehavior passHref>
-                    <NavigationMenuLink className="no-underline font-medium">
-                      Unsere KI
-                      <p className="text-xs font-normal">
-                        Wir bauen auf Transparenz un übernehmen Verantwortung
-                        für unsere KI
-                      </p>
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            {/* <NavigationMenuItem>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="list-none bg-zinc-800">
+                  <NavigationMenuItem className="p-4 w-96">
+                    <Link href="#how-does-it-work" legacyBehavior passHref>
+                      <NavigationMenuLink className="no-underline font-medium">
+                        Wie funktioniert es?
+                        <p className="text-xs font-normal">
+                          Wie ist der Workflow von der Anfrage bis zum Ergebnis?
+                        </p>
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem className="p-4 w-96 pt-0">
+                    <Link href="/#our-mission" legacyBehavior passHref>
+                      <NavigationMenuLink className="no-underline font-medium">
+                        Das macht uns besonderns
+                        <p className="text-xs font-normal">
+                          Wir wollen Unternehmen in allen Belangen eine Hilfe
+                          sein für Ihren Erfolg.
+                        </p>
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem className="p-4 w-96 pt-0">
+                    <Link href="#our-ai" legacyBehavior passHref>
+                      <NavigationMenuLink className="no-underline font-medium">
+                        Unsere KI
+                        <p className="text-xs font-normal">
+                          Wir bauen auf Transparenz un übernehmen Verantwortung
+                          für unsere KI
+                        </p>
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              {/* <NavigationMenuItem>
               <Link href="/docs" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   Documentation
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem> */}
-          </NavigationMenuList>
-        </NavigationMenu>
-        <C2AButton
-          className="lg:w-[10rem] w-full h-10 text-sm align-middle p-0 justify-center items-center flex px-2 absolute right-4 text-black"
-          href="/contact"
-        >
-          Anruf vereinbaren
-        </C2AButton>
-      </div>}
+            </NavigationMenuList>
+          </NavigationMenu>
+          <C2AButton
+            className="lg:w-[10rem] w-full h-10 text-sm align-middle p-0 justify-center items-center flex px-2 absolute right-4 text-black"
+            href="/contact"
+          >
+            Anruf vereinbaren
+          </C2AButton>
+        </div>
+      )}
     </header>
   );
 };
